60d30aa050a09e37cf66f1948027f22c3cdc1c55
Frankie Seabrook <fifthfrankie@gmail.com>
2026-01-11 23:14:52 +0000

    refactor: add Constants.cs for config values

---
+++ Constants.cs
@@ -0,0 +1,11 @@
+public static class Constants
+{
+    // How long a work session lasts in seconds (25 mins)
+    public const int WorkDuration = 25 * 60;
+    // How long a break lasts in seconds (5 mins)
+    public const int BreakDuration = 5 * 60;
+    // How often timer checks in milliseconds
+    public const int UpdateInterval = 1000;
+    // How to show the time on screen
+    public const string TimerFormat = "mm\\:ss";
+}
